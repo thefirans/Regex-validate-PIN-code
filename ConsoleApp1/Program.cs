d31628197ade096d12cb11198e4b1c18198d6572
Sasha <sleo170504@gmail.com>
2022-09-30 11:55:11 +0300

    Add new files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Kata
+    {
+        public static bool ValidatePin(string pin)
+        {
+            bool result = false;
+            if (pin.Length == 4 || pin.Length == 6)
+            {
+                if (Regex.IsMatch(pin, @"[0-9]{4}{6}"))
+                {
+                    result = true;
+                }
+                else result = false;
+            }
+            else result = false;
+            return result;
+        }
+    }
+}
---
+++ TestProject18/UnitTest1.cs
@@ -0,0 +1,43 @@
+using ConsoleApp1;
+namespace TestProject18
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test, Description("ValidatePin should return false for pins with length other than 4 or 6")]
+            public void LengthTest()
+            {
+                Assert.AreEqual(false, Kata.ValidatePin("1"), "Wrong output for \"1\"");
+                Assert.AreEqual(false, Kata.ValidatePin("12"), "Wrong output for \"12\"");
+                Assert.AreEqual(false, Kata.ValidatePin("123"), "Wrong output for \"123\"");
+                Assert.AreEqual(false, Kata.ValidatePin("12345"), "Wrong output for \"12345\"");
+                Assert.AreEqual(false, Kata.ValidatePin("1234567"), "Wrong output for \"1234567\"");
+                Assert.AreEqual(false, Kata.ValidatePin("-1234"), "Wrong output for \"-1234\"");
+                Assert.AreEqual(false, Kata.ValidatePin("1.234"), "Wrong output for \"1.234\"");
+                Assert.AreEqual(false, Kata.ValidatePin("-1.234"), "Wrong output for \"-1.234\"");
+                Assert.AreEqual(false, Kata.ValidatePin("00000000"), "Wrong output for \"00000000\"");
+            }
+
+            [Test, Description("ValidatePin should return false for pins which contain characters other than digits")]
+            public void NonDigitTest()
+            {
+                Assert.AreEqual(false, Kata.ValidatePin("a234"), "Wrong output for \"a234\"");
+                Assert.AreEqual(false, Kata.ValidatePin(".234"), "Wrong output for \".234\"");
+            }
+
+            [Test, Description("ValidatePin should return true for valid pins")]
+            public void ValidTest()
+            {
+                Assert.AreEqual(true, Kata.ValidatePin("1234"), "Wrong output for \"1234\"");
+                Assert.AreEqual(true, Kata.ValidatePin("0000"), "Wrong output for \"0000\"");
+                Assert.AreEqual(true, Kata.ValidatePin("1111"), "Wrong output for \"1111\"");
+                Assert.AreEqual(true, Kata.ValidatePin("123456"), "Wrong output for \"123456\"");
+                Assert.AreEqual(true, Kata.ValidatePin("098765"), "Wrong output for \"098765\"");
+                Assert.AreEqual(true, Kata.ValidatePin("000000"), "Wrong output for \"000000\"");
+                Assert.AreEqual(true, Kata.ValidatePin("090909"), "Wrong output for \"090909\"");
+            }
+        }
+    }
+}
